0708285ac49b5672cc705708880cc516d7a46dbd
Will Sherrer <wsherre@clemson.edu>
2021-02-08 00:07:23 -0500

    backup

---
+++ HauntedHouse/Assets/test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Light yo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y >= 1.5)
+        {
+            yo.intensity = 8;
+        }
+        else
+        {
+            yo.intensity = 0.1f;
+        }
+    }
+}
