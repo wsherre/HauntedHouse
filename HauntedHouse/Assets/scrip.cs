8c6f182d0e3971f44f5cbc05a5adfe37650b5324
Chris Gaber <cgaber@clemson.edu>
2021-02-10 14:35:53 -0500

    Chris's Work for Milestone 1

---
+++ HauntedHouse/Assets/scrip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrip : MonoBehaviour
+{
+    public int rotationAmt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void rotateObject()
+    {
+        transform.rotation *= Quaternion.Euler(rotationAmt, 0, 0);
+    }
+
+}
